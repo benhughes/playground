a43592dca40005ee7bc6e8006aef4c1a2f94b820
Ben Hughes <benhuggy@gmail.com>
2013-02-09 13:31:12 +0000

    Playing around with promises with some basic examples added

---
+++ promises-examples/js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+    //basicExample();
+    realWorldExample();
+
+
+
+})
+
+function basicExample () {
+    console.log('hi');
+    var deferred = new $.Deferred();
+    deferred.done(function(data) { console.log(data) });
+    deferred.fail(function(data) { console.log('fail', data) });
+    setTimeout(function () {
+        //resolve it and pass the data
+        deferred.resolve('some data');
+        deferred.reject('some data');
+    }, 3000);
+    setTimeout(function () {
+        console.log(deferred.state());
+    }, 2000);
+    setTimeout(function () {
+        console.log(deferred.state());
+    }, 4000);
+}
+
+function realWorldExample () {
+    $.when(execution()).then(executionDone);
+
+    function execution(data) {
+        var dfd = $.Deferred();
+        console.log('start execution');
+
+        //in the real world, this would probably make an AJAX call.
+        setTimeout(function() { dfd.resolve() }, 2000);
+
+        return dfd.promise();
+    }
+
+    function executionDone(){
+        console.log('execution ended');
+    }
+}
